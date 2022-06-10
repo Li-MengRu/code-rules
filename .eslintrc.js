76cf4ae38c7234f102b5f895b36950e05cd924b6
Li-MengRu <li_mengru@yeah.net>
2022-06-10 15:33:49 +0800

    feat: 🎸 version-0.1.3 模板修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,16 +22,15 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    quotes: ['error', 'single'],
+    quotes: ['warn', 'double'],
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/ban-ts-comment': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
     'block-scoped-var': 2, // var变量提升
-    'block-spacing': 0, // 语句之间用强制使用空格隔开
-    allowSingleLine: 0,
+    'block-spacing': 1, // 语句之间用强制使用空格隔开
     'no-unused-vars': 1,
     eqeqeq: 1, // 使用强等号
-    'func-call-spacing': ['error'], // 函数名和（）之间放在一行，不换行
+    'func-call-spacing': ['error'], // 不允许函数名和调用它的左括号之间有空格
     'global-require': 1, // require语句提升到文件顶部
     indent: ['error', 2], // 缩进两格
     complexity: ['error', 6], // 控制圈复杂度不能超过6
@@ -40,31 +39,27 @@ module.exports = {
     'max-nested-callbacks': ['error', 3], // 回调函数最多嵌套三层
     'multiline-comment-style': ['error', 'starred-block'], // 备注信息两行双斜杠的合并成/* */
     'new-parens': 1, // new关键字调用不带参数的构造函数时增加（）
-    'newline-per-chained-call': 1, // 调用链接方法换行
+    'newline-per-chained-call': 1, // 链式调用方法换行
     'no-await-in-loop': 1, // for循环里不能有await，建议使用promise.all（）
     'no-const-assign': 1, // const不能被修改
-    'no-duplicate-imports': 1, // 合并导入   没生效
-    'no-else-return': 1, // if语句中return后不需要else
-    'no-empty': 2, // 不允许空块语句
-    'no-empty-function': 0, // 消除空功能
-    'no-extra-semi': 1, // 替换不必要的分号
-    'no-fallthrough': 1, // switch语句中缺少的break
+    'no-duplicate-imports': ["error", { "includeExports": true }], // 合并导入
+    'no-else-return': 2, // if语句中return后不需要else
+    'no-empty': 1, // 块语句中的内容不能为空
+    'no-empty-function': 1, // 消除空功能
+    'no-extra-semi': 2, // 替换不必要的分号
+    'no-fallthrough': 2, // switch语句中缺少的break
     'no-floating-decimal': 1, // 小数点前后缺少数字时警告
-    'no-lonely-if': 1, // if else 语句合并
-    'no-multi-assign': 1, // 禁止链接变量赋值
+    'no-lonely-if': 2, // else if 语句合并
+    'no-multi-assign': 2, // 禁止链式赋值
     'no-multiple-empty-lines': 1, // 最多两行空行
-    'no-nested-ternary': 1, // 不允许嵌套三元表达式
-    'no-proto': 1, // __proto__弃用，改用getPrototypeOf
+    'no-nested-ternary': 2, // 不允许嵌套三元表达式
+    'no-proto': 2, // __proto__弃用，改用getPrototypeOf
     'no-undef-init': 1, // 消除初始化变量值为undefined的
-    'no-var': 1, // 声明变量尽量使用let const
+    'no-var': 2, // 声明变量尽量使用let const
     'object-shorthand': 1, // 简化对象字面值方法和属性
     'prefer-template': 1, // 拼接字符串用模板文字而不是字符串串联
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] }
-    ],
-    'no-shadow': 'off',
+    'no-shadow': 2,
     'no-continue': 1,
-    'comma-dangle': ['error', 'always']
+    'comma-dangle': ['error', 'only-multiline'] // 禁止使用尾逗号
   }
 };
